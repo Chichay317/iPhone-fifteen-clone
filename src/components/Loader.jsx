6b995bdd741b6e823e03fd9443fd9296caa27dc1
Divine <apple@APPLEs-MacBook-Pro.local>
2024-05-09 15:43:15 +0100

    added spinner for the model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,16 @@
-import { Html, useGLTF } from "@react-three/drei";
+import React from "react";
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
-  const { scene } = useGLTF("./models/loading/scene.gltf");
-
-  console.log(scene);
-
   return (
-    <mesh>
-      <primitive object={scene} scale={0.4} position={[0, -3.25, -1.5]} />
-    </mesh>
+    <Html>
+      <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <div class="relative">
+          <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-blue"></div>
+          <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-red-500 animate-spin"></div>
+        </div>
+      </div>
+    </Html>
   );
 };
 
